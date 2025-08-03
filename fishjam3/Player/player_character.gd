16549efb4a6e399e3b33d64e44b79b6be1bba553
LÓ-B-ster <larsborda@googlemail.com>
2025-08-04 00:15:43 +0200

    removed the stupid gravity that was just one fucking line

--- fishjam3/Player/player_character.gd
+++ fishjam3/Player/player_character.gd
@@ -7,7 +7,6 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	print(get_gravity())
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
