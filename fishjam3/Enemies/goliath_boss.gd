39f979bf858762596c8e05e7b0e47e00fdfc546a
LÓ-B-ster <larsborda@googlemail.com>
2025-08-08 18:34:24 +0200

    broken spear attack "animation"

--- fishjam3/Enemies/goliath_boss.gd
+++ fishjam3/Enemies/goliath_boss.gd
@@ -18,13 +18,24 @@ func _process(delta: float) -> void:
 
 func spear_attack():
 	print("Spear Attack!")
+	spear_area.set_collision_layer(4)
+	var player_position = get_player.get_global_position()
+	# gets the angle we want to face
+	var angle_to_player = global_position.direction_to(player_position).angle()
+	
+	# slowly changes the rotation to face the angle
+	spear_area.rotation = move_toward(rotation, angle_to_player, 1)
+	
+	#spear_area.global_position.x = move_toward(spear_area.global_position.x, player_position.x, 50)
+	#spear_area.global_position.y = move_toward(spear_area.global_position.y, player_position.x, 50)
 	is_attacking = false
 	
 func sword_attack():
 	print("Sword Attack!")
 	is_attacking = false
 func _on_attack_timer_timeout() -> void:
-	if is_attacking == false:
+	var distance = get_player.global_position.distance_to(global_position)
+	if is_attacking == false && distance <= 100:
 		is_attacking = true
 		var attack_choice = randf()
 		var attack = randf()
